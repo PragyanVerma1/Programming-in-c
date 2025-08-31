b28c68e91bfe162346b95dc73e16485163f1cfa0
PragyanVerma1 <pragyanverma91@gmail.com>
2025-08-31 20:21:34 +0530

    EXP1.1.c

--- EXP1.1.c
+++ EXP1.1.c
@@ -1,60 +1,11 @@
 //EXPERIMENT 1: Installation, Environmebnt Setup and starting with C language
 
 //1.Write a program to print hello world
-/*#include<stdio.h>
+#include<stdio.h>
 int main(){
     prinf("Hello World");
     return 0;
-}*/
-
-
-
-
-
-
-//2. Write a program to print address in next line
-/*#include<stdio.h>
-int main(){
-    printf("Belleza Boys Hostel,\nDehradun");
-    return 0;
-}*/
-
-
-
-
-//4. Write to program to get the sum of two variables
-/*#include<stdio.h>
-int main(){
-    int a, b;
-    printf("Enter the value of a : ");
-    scanf("%d", &a);
-    printf("Enter the value of b : ");
-    scanf("%d", &b);
-    int sum = a+b;
-    printf("The sum is : %d", sum);
-    return 0;
-    
-}*/
-
-
-
-
-//Write a program in c to perform 4 arithmetic opertions on 2 variables
-/*#include<stdio.h>
-int main() {
-    int a, b;
-    printf("Enter the value of a : ");
-    scanf("%d", &a);
-    printf("Enter the value of b : ");
-    scanf("%d", &b);
-    printf("The sum is %d", a+b 
-    );
-    printf("The subtraction is %d", a-b);
-    printf("THe multiplication is %d", a*b);
-    float div = a/b;
-    printf("The division is %f", div);
-    return 0;
-}*/
+}
 
 
 
