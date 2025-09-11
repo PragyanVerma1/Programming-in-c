9534825e361de20eb41785af5ace80c0bafd4849
PragyanVerma1 <pragyanverma91@gmail.com>
2025-09-11 13:36:14 +0530

    Add files via upload

---
+++ Exp2.2.c
@@ -0,0 +1,22 @@
+//2. WAP a C program to Convert temperature from Celsius to Fahrenheit using the
+//formula: F = (C * 9/5) + 32. 
+#include<stdio.h>
+int main() {
+	float c;
+	printf("Name- Pragyan Verma\nSAP ID - 590028276\nCourse- BCA\nBatch- B5");
+    printf("\n-----------------\n");
+	printf("Enter value of c : ");
+	scanf("%f", &c);
+	float F= (c*(9/5)) + 32;
+	printf("The temp in fahrenhiet is %f", F);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
