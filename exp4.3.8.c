fbdaff5adf5dda801a4a3a8e1b82f71581e332c0
PragyanVerma1 <pragyanverma91@gmail.com>
2025-10-28 20:20:39 +0530

    exp4.3.8.c

---
+++ exp4.3.8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int n = 5, i, j;
+    
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n - i; j++)
+            printf(" ");
+        for(j = n; j >= n - i + 1; j--)
+            printf("%d ", j);
+        for(j = n - i + 2; j <= n; j++)
+            printf("%d ", j);
+        printf("\n");
+    }
+
+    for(i = n - 1; i >= 1; i--) {
+        for(j = 1; j <= n - i; j++)
+            printf(" ");
+        for(j = n; j >= n - i + 1; j--)
+            printf("%d ", j);
+        for(j = n - i + 2; j <= n; j++)
+            printf("%d ", j);
+        printf("\n");
+    }
+    return 0;
+}
