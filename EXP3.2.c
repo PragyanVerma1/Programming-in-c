e8500e0bc7abc19c2ac5a5671aa04c55744f2062
PragyanVerma1 <pragyanverma91@gmail.com>
2025-10-28 17:30:17 +0530

    EXP3.2.c

---
+++ EXP3.2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c;
+    printf("Enter the three sides of the triangle: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a + b > c && a + c > b && b + c > a) {
+        printf("The triangle is valid.\n");
+
+        if (a == b && b == c)
+            printf("It is an Equilateral triangle.\n");
+        else if (a == b || b == c || a == c)
+            printf("It is an Isosceles triangle.\n");
+        else
+            printf("It is a Scalene triangle.\n");
+
+        if ((a * a + b * b == c * c) ||
+            (a * a + c * c == b * b) ||
+            (b * b + c * c == a * a))
+            printf("It is also a Right-angled triangle.\n");
+    } else {
+        printf("The triangle is not valid.\n");
+    }
+
+    return 0;
+}
