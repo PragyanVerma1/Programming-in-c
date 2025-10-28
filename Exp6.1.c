f246238098c5dd3b3340499221e51f29631f0246
PragyanVerma1 <pragyanverma91@gmail.com>
2025-10-28 20:25:38 +0530

    Exp6.1.c

---
+++ Exp6.1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int n, i, largest, second_largest;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    if(arr[0] > arr[1]) {
+        largest = arr[0];
+        second_largest = arr[1];
+    } else {
+        largest = arr[1];
+        second_largest = arr[0];
+    }
+    for(i = 2; i < n; i++) {
+        if(arr[i] > largest) {
+            second_largest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > second_largest && arr[i] != largest) {
+            second_largest = arr[i];
+        }
+    }
+    printf("Second largest element is: %d\n", second_largest);
+    return 0;
+}
